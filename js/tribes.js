0d62329085ccfb9333d96d7f0ee64616b75d03ed
XPS-15\Sean Yu <seanyu4296@gmail.com>
2017-03-29 22:58:47 +0800

    Add new css and new html files for tribes

---
+++ js/tribes.js
@@ -0,0 +1,8 @@
+$('#to-weaves').on('click', function (e) {
+  e.preventDefault();
+  window.location.assign("/#weaves-fold");
+})
+$('#to-weavers').on('click', function (e) {
+  e.preventDefault();
+  window.location.assign("/#weavers-fold");
+})
